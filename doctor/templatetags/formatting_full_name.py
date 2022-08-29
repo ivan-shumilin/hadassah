643eb844900e2a1f0b75c4c115d79171ca511999
ivan-shumiln <shumilin.i.v@gmail.com>
2022-08-29 15:06:32 +0300

    Добавил тег, который приводит ФИО в нужный формат.

---
+++ doctor/templatetags/formatting_full_name.py
@@ -0,0 +1,21 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter
+@stringfilter
+def formatting_full_name(full_name):
+    list = full_name.split()
+    res = ''
+    for index, value in enumerate(list):
+        print(index, len(list))
+        if index == 0:
+            res += value.capitalize() + ' '
+            continue
+        if index == len(list) - 1:
+            res += value[0:1].capitalize()
+            continue
+        else:
+            res += value[0:1].capitalize() + '.'
+    return res
