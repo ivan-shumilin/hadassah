929c950e210dd0dcadaf914394ec7fbc89037cd2
ivan-shumiln <shumilin.i.v@gmail.com>
2022-10-21 16:15:09 +0300

    Update

--- nutritionist/views.py
+++ nutritionist/views.py
@@ -997,8 +997,8 @@ def printed_form_two_cafe(request):
     formatted_date_now = dateformat.format(date.fromisoformat(str(date.today())), 'd E, l')
     time_now = str(datetime.today().time().hour) + ':' + str(datetime.today().time().minute)
     # какой прием пищи
-    # meal = what_meal()
-    meal = 'lunch'
+    meal = what_meal()
+    # meal = 'lunch'
     catalog = {}
 
     users = CustomUser.objects.all()
