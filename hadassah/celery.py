aec48cc34d60eb85044b97e5295b277a9d01f494
ivan-shumiln <shumilin.i.v@gmail.com>
2022-10-25 13:14:55 +0300

    Update

--- hadassah/celery.py
+++ hadassah/celery.py
@@ -10,29 +10,25 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'my_job_applies_changes_1': {
-        'task': 'doctor.tasks.my_job_applies_changes_breakfast',
-        'schedule': crontab(minute=5, hour='13'),
-    },
     'my_job_applies_changes_2': {
         'task': 'doctor.tasks.my_job_applies_changes_breakfast',
-        'schedule': crontab(minute=6, hour='13'),
+        'schedule': crontab(minute=17, hour='13'),
     },
     'my_job_applies_changes_3': {
         'task': 'doctor.tasks.my_job_applies_changes_breakfast',
-        'schedule': crontab(minute=7, hour='13'),
+        'schedule': crontab(minute=18, hour='13'),
     },
     'my_job_applies_changes_4': {
         'task': 'doctor.tasks.my_job_applies_changes_breakfast',
-        'schedule': crontab(minute=8, hour='13'),
+        'schedule': crontab(minute=19, hour='13'),
     },
     'my_job_applies_changes_5': {
         'task': 'doctor.tasks.my_job_applies_changes_breakfast',
-        'schedule': crontab(minute=9, hour='13'),
+        'schedule': crontab(minute=20, hour='13'),
     },
     'my_job_applies_changes_6': {
         'task': 'doctor.tasks.my_job_applies_changes_breakfast',
-        'schedule': crontab(minute=10, hour='13'),
+        'schedule': crontab(minute=21, hour='13'),
     },
 }
 app.conf.timezone = 'Europe/Moscow'
