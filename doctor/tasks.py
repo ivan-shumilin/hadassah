5a060b942fb2791dedfceabe4cc3074d663dba27
ivan-shumiln <shumilin.i.v@gmail.com>
2022-10-25 13:00:29 +0300

    Update

--- doctor/tasks.py
+++ doctor/tasks.py
@@ -1,13 +1,62 @@
 from celery import shared_task
 import telepot
 from nutritionist.models import BotChatId
+from doctor.functions.for_print_forms import create_user_today, applies_changes, create_user_tomorrow
+
+
+def send_messang(meal):
+    check = '\u2705'
+    TOKEN = '5533289712:AAEENvPBVrfXJH1xotRzoCCi24xFcoH9NY8'
+    bot = telepot.Bot(TOKEN)
+    # все номера chat_id
+    messang = f'{check} Заказ на <u><b>{meal}</b></u> сформирован.'
+    for item in BotChatId.objects.all():
+        bot.sendMessage(item.chat_id, messang, parse_mode="html")
 
 @shared_task()
-def send_messang():
+def my_job_applies_changes_():
+    applies_changes()
+
+@shared_task()
+def my_job_applies_changes_breakfast():
+    # applies_changes() # накатываем изменения
+    send_messang('завтрак')
+
+@shared_task()
+def my_job_applies_changes_lunch():
+    applies_changes() # накатываем изменения
+    send_messang('обед')
+
+@shared_task()
+def my_job_applies_changes_afternoon():
+    applies_changes() # накатываем изменения
+    send_messang('полдник')
+
+@shared_task()
+def my_job_applies_changes_dinner():
+    applies_changes() # накатываем изменения
+    send_messang('ужин')
+
+@shared_task()
+def my_job_create_user_today():
+    create_user_today() # создаем таблицу с пользователями на сегодня
+
+@shared_task()
+def my_job_create_user_tomorrow():
+    """ Создаем таблицу с пользователями на завтра """
+    create_user_tomorrow()  # создаем таблицу с пользователями на завтра
+    # applies_changes()
+    send_messang('завтра')
+
+
+
+@shared_task()
+def send_messang_1():
     TOKEN = '5533289712:AAEENvPBVrfXJH1xotRzoCCi24xFcoH9NY8'
     attention = u'\u2757\ufe0f'
     bot = telepot.Bot(TOKEN)
     # все номера chat_id
-    messang = f'test'
+    messang = f'test_1'
     for item in BotChatId.objects.all():
         bot.sendMessage(item.chat_id, messang, parse_mode="html")
+
--- hadassah/celery.py
+++ hadassah/celery.py
@@ -1,7 +1,38 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hadassah.settings")
 app = Celery("hadassah")
+
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'my_job_applies_changes_1': {
+        'task': 'doctor.tasks.my_job_applies_changes_breakfast',
+        'schedule': crontab(minute=5, hour='13'),
+    },
+    'my_job_applies_changes_2': {
+        'task': 'doctor.tasks.my_job_applies_changes_breakfast',
+        'schedule': crontab(minute=6, hour='13'),
+    },
+    'my_job_applies_changes_3': {
+        'task': 'doctor.tasks.my_job_applies_changes_breakfast',
+        'schedule': crontab(minute=7, hour='13'),
+    },
+    'my_job_applies_changes_4': {
+        'task': 'doctor.tasks.my_job_applies_changes_breakfast',
+        'schedule': crontab(minute=8, hour='13'),
+    },
+    'my_job_applies_changes_5': {
+        'task': 'doctor.tasks.my_job_applies_changes_breakfast',
+        'schedule': crontab(minute=9, hour='13'),
+    },
+    'my_job_applies_changes_6': {
+        'task': 'doctor.tasks.my_job_applies_changes_breakfast',
+        'schedule': crontab(minute=10, hour='13'),
+    },
+}
+app.conf.timezone = 'Europe/Moscow'
