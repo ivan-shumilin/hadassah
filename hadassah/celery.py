2189e1abc1db8e37652efc3246abceaedadf2090
ivan-shumiln <shumilin.i.v@gmail.com>
2022-10-25 13:21:53 +0300

    Update

--- hadassah/celery.py
+++ hadassah/celery.py
@@ -12,23 +12,23 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'my_job_applies_changes_2': {
         'task': 'doctor.tasks.my_job_applies_changes_breakfast',
-        'schedule': crontab(minute=17, hour='13'),
+        'schedule': crontab(minute=23, hour='13'),
     },
     'my_job_applies_changes_3': {
         'task': 'doctor.tasks.my_job_applies_changes_breakfast',
-        'schedule': crontab(minute=18, hour='13'),
+        'schedule': crontab(minute=24, hour='13'),
     },
     'my_job_applies_changes_4': {
         'task': 'doctor.tasks.my_job_applies_changes_breakfast',
-        'schedule': crontab(minute=19, hour='13'),
+        'schedule': crontab(minute=25, hour='13'),
     },
     'my_job_applies_changes_5': {
         'task': 'doctor.tasks.my_job_applies_changes_breakfast',
-        'schedule': crontab(minute=20, hour='13'),
+        'schedule': crontab(minute=26, hour='13'),
     },
     'my_job_applies_changes_6': {
         'task': 'doctor.tasks.my_job_applies_changes_breakfast',
-        'schedule': crontab(minute=21, hour='13'),
+        'schedule': crontab(minute=27, hour='13'),
     },
 }
 app.conf.timezone = 'Europe/Moscow'
