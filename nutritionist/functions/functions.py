9050aa3acee3a27354151bc78d66a840b4641d7f
ivan-shumiln <shumilin.i.v@gmail.com>
2023-03-16 11:53:52 +0300

    В стикеры добавил этаж.

--- nutritionist/functions/functions.py
+++ nutritionist/functions/functions.py
@@ -120,8 +120,27 @@ def create_stickers_pdf(catalog):
             pdf.set_right_margin(0)
             pdf.add_page()
             pdf.add_font("Arial1", "", "FontsFree-Net-arial-bold.ttf", uni=True)
-            pdf.set_font("Arial1", style='', size=33)
+            pdf.set_font("Arial1", style='', size=45)
             ln = 1
+            if floor == 'users_2nd_floor':
+                print_floor = 'Этаж 2'
+            elif floor == 'users_3nd_floor':
+                print_floor = 'Этаж 3'
+            elif floor == 'users_4nd_floor':
+                print_floor = 'Этаж 4'
+            else:
+                print_floor = ''
+            department = item['department'] if item["department"] != "Не выбрано" else ""
+            department = 'Терапия' if department == 'Онкология' else department
+            if department and print_floor:
+                head_top = print_floor + ", " + department
+            else:
+                head_top = print_floor + department
+            if head_top:
+                pdf.cell(50, 14, txt=f'{head_top}', ln=ln, align="L")
+                ln += 1
+            pdf.add_font("Arial1", "", "FontsFree-Net-arial-bold.ttf", uni=True)
+            pdf.set_font("Arial1", style='', size=33)
             head = f'{formatting_full_name(item["name"])} {", " + item["room_number"] if item["room_number"] != "Не выбрано" else ""} {"" if item["bed"] == "Не выбрано" or item["room_number"] == "Не выбрано" else ", " + item["bed"]}'
             head = head.strip()
             pdf.cell(50, 14, txt=f'{head}', ln=ln, align="L")
