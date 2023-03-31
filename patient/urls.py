42679324ec2efa70f1ae08bbc5e7362fab1f9381
ivan-shumiln <shumilin.i.v@gmail.com>
2023-03-31 21:00:13 +0300

    fix: Настройка PWA.

--- patient/urls.py
+++ patient/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.user_login, name='patient-login'),
     path('history/<id>', views.patient_history, name='patient_history'),
     path('history/test/test', views.patient_history_test, name='patient_history_test'),
+    path('<id>', views.patient, name='patient'),
     path('api/v1/submitselection', views.SubmitPatientSelectionAPIView.as_view()),
     path('api/v1/login/datavalidation', views.loginDataValidationAPIView.as_view())
     ]
