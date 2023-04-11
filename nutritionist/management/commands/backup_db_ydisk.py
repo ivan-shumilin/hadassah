e16aa28aa03d3c9e90370b9117aac82b82235533
ivan-shumiln <shumilin.i.v@gmail.com>
2023-04-11 19:14:56 +0300

    fix: добавил в backup все модели.

--- nutritionist/management/commands/backup_db_ydisk.py
+++ nutritionist/management/commands/backup_db_ydisk.py
@@ -37,6 +37,7 @@ def create_backup():
                                 'nutritionist.report',
                                 'nutritionist.productstorage',
                                 'nutritionist.ingredient',
+                                'django.contrib.auth.group',
                                 stdout=outfile)
 
 
