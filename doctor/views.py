adc8eb17d78f51a47fe6efa58c84272f40cf53ba
shimilin <shumilin.i.v@gmail.com>
2023-06-27 13:33:07 +0300

    fix: изменил телеграм чат id.

--- doctor/views.py
+++ doctor/views.py
@@ -604,7 +604,7 @@ class SendPatientProductsAPIView(APIView):
 
 
         messang += f'({user_name})'
-        send_messang_changes(messang, '-658303105')
+        send_messang_changes(messang, '-1001913194437')
         # my_job_send_messang_changes.delay(messang, '-658303105')
 
         return Response('ok')
@@ -671,7 +671,7 @@ class SendEmergencyFoodAPIView(APIView):
                    type_of_diet=patient.type_of_diet).save()
 
         messang += f'({user_name})'
-        send_messang_changes(messang, '-658303105')
+        send_messang_changes(messang, '-1001913194437')
         return Response('ok')
 
 
