ea06e19336818155ee85ea16bdca921c0ff5a332
shimilin <shumilin.i.v@gmail.com>
2023-10-27 17:11:00 +0300

    fix: Добавил новое приложение "report".

--- scripts/copy_diets.py
+++ scripts/copy_diets.py
@@ -12,18 +12,26 @@ def copy_diet():
     days = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
     meals = ['breakfast', 'lunch', 'afternoon', 'dinner']
     # meals = ['dinner']
-    # diets = ['ОВД', 'ОВД без сахара', 'ОВД веган (пост) без глютена', 'Нулевая диета', 'БД день 1', 'БД день 2',
-    #          'ЩД', 'ЩД без сахара', 'НБД', 'ВБД', 'НКД', 'ВКД', 'Безйодовая', 'ПЭТ/КТ', 'Без ограничений']
-    diets = ['Нулевая диета']
-    diet_change = 'ОВД (Э)'
+    diets = ['ОВД', 'ОВД без сахара', 'ОВД веган (пост) без глютена', 'Нулевая диета', 'БД день 1', 'БД день 2', 'БД',
+              'ЩД', 'ЩД без сахара', 'НБД', 'ВБД', 'НКД', 'ВКД', 'Безйодовая', 'ПЭТ/КТ', 'Без ограничений']
+    # diets = ['БД']
+
     for diet in diets:
+        diet_change1 = f'{diet} (Э)'
+        diet_change2 = f'{diet} (П)'
         for day in days:
             for meal in meals:
                 protduct_set = TimetableLp.objects.filter(type_of_diet=diet, day_of_the_week=day, meals=meal)
                 for product in protduct_set:
                     to_create.append(TimetableLp(
                         item=product.item,
-                        type_of_diet=diet_change,
+                        type_of_diet=diet_change1,
+                        day_of_the_week=day,
+                        meals=meal,
+                    ))
+                    to_create.append(TimetableLp(
+                        item=product.item,
+                        type_of_diet=diet_change2,
                         day_of_the_week=day,
                         meals=meal,
                     ))
