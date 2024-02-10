6e2081f71019a5aedd6bd9d0dd6f5ac8578630fc
alice <alisaageeva311@gmail.com>
2024-02-10 15:38:42 +0300

    [добавила тестов для create_user]

--- doctor/tests/test_check_correct_user_create.py
+++ doctor/tests/test_check_correct_user_create.py
@@ -4,45 +4,21 @@ from unittest.mock import patch
 from django.test import RequestFactory
 
 from doctor.forms import PatientRegistrationForm
-from nutritionist.models import CustomUser
-from doctor.functions.functions import create_user
+from doctor.functions.for_print_forms import update_UsersToday
+from doctor.functions.helpers import formatting_full_name_mode_full
+from nutritionist.models import CustomUser, UsersToday
+from doctor.functions.functions import create_user, is_user_look
 
 
 class CustomUserCreateTests(unittest.TestCase):
-    """ Полное тестирование функции create_user со всеми внутренними функциями.
-     Warning!!! Для корректной работы тестов на данном этапе необходимо закомментировать импорт и
-     все использования функции from doctor.tasks import my_job_send_messang_changes """
+    """Полное тестирование функции create_user со всеми внутренними функциями.
+    Warning!!! Для корректной работы тестов на данном этапе необходимо закомментировать импорт и
+    все использования функции from doctor.tasks import my_job_send_messang_changes"""
 
     def setUp(self):
         self.factory = RequestFactory()
         self.user = None
-
-    @patch("doctor.functions.functions.get_user_name")
-    @patch('doctor.functions.functions.logging')
-    @patch("doctor.functions.functions.add_the_patient_menu")
-    @patch("doctor.functions.functions.update_UsersToday")
-    # @patch("doctor.functions.functions.check_meal_user")
-    # @patch("doctor.functions.functions.is_user_look")
-    @patch("doctor.functions.functions.get_now_show_meal")
-    @patch("doctor.functions.functions.do_messang_send")
-    @patch('doctor.functions.functions.add_the_patient_menu')
-    @patch("doctor.functions.functions.add_features")
-    @patch("doctor.functions.functions.check_change")
-    def test_correct_user_save(
-        self,
-        mock_get_user_name,
-        mock_logging,
-        mock_add_the_patient_menu,
-        mock_update_users_today,
-        # mock_is_user_look,
-        # mock_check_meal_user,
-        mock_get_now_show_meal,
-        mock_do_messang_send,
-        mock_formatting_full_name,
-        mock_add_features,
-        mock_check_change,
-    ):
-        data = {
+        self.data = {
             "add_patient": "True",
             "full_name": "Егорова Светлана Степановна",
             "birthdate": "09.05.1990",
@@ -70,19 +46,44 @@ class CustomUserCreateTests(unittest.TestCase):
             "dinner_add": "False",
         }
 
+    @patch("doctor.functions.functions.get_user_name")
+    @patch("doctor.functions.functions.logging")
+    @patch("doctor.functions.functions.add_the_patient_menu")
+    @patch("doctor.functions.functions.update_UsersToday")
+    # @patch("doctor.functions.functions.check_meal_user")
+    # @patch("doctor.functions.functions.is_user_look")
+    @patch("doctor.functions.functions.get_now_show_meal")
+    @patch("doctor.functions.functions.do_messang_send")
+    @patch("doctor.functions.functions.add_the_patient_menu")
+    @patch("doctor.functions.functions.add_features")
+    @patch("doctor.functions.functions.check_change")
+    def test_correct_user_save(
+        self,
+        mock_get_user_name,
+        mock_logging,
+        mock_add_the_patient_menu,
+        mock_update_users_today,
+        # mock_is_user_look,
+        # mock_check_meal_user,
+        mock_get_now_show_meal,
+        mock_do_messang_send,
+        mock_formatting_full_name,
+        mock_add_features,
+        mock_check_change,
+    ):
         mock_logging.info.return_value = None
         mock_add_the_patient_menu.return_value = None
         mock_update_users_today.return_value = None
         mock_get_now_show_meal.return_value = "ужина"
         mock_do_messang_send.return_value = True
         mock_formatting_full_name.return_value = "Егорова Антонина Степановна"
-        mock_add_features.return_value = 'Питание через зонд.'
-        mock_check_change.return_value = 'завтрака', 0
+        mock_add_features.return_value = "Питание через зонд."
+        mock_check_change.return_value = "завтрака", 0
 
         mock_get_user_name.return_value = ""
         count_after_add_user = CustomUser.objects.count()
 
-        request = self.factory.post("/doctor/", data=data)
+        request = self.factory.post("/doctor/", data=self.data)
         request.user = self.user
         user_form = PatientRegistrationForm(request.POST)
         create_user(user_form, request)
@@ -93,26 +94,32 @@ class CustomUserCreateTests(unittest.TestCase):
         self.assertEqual(count_after_add_user, count - 1)
 
         # проверка, что данные корректно сохранились
-        self.assertEqual(current_user.full_name, data["full_name"])
-        self.assertEqual(current_user.birthdate.strftime("%d.%m.%Y"), data["birthdate"])
-        self.assertEqual(current_user.receipt_date.strftime("%d.%m.%Y"), data["receipt_date"])
-        self.assertEqual(current_user.receipt_time.strftime("%H:%M:%S"), data["receipt_time"])
+        self.assertEqual(current_user.full_name, self.data["full_name"])
+        self.assertEqual(
+            current_user.birthdate.strftime("%d.%m.%Y"), self.data["birthdate"]
+        )
+        self.assertEqual(
+            current_user.receipt_date.strftime("%d.%m.%Y"), self.data["receipt_date"]
+        )
+        self.assertEqual(
+            current_user.receipt_time.strftime("%H:%M:%S"), self.data["receipt_time"]
+        )
 
-        self.assertEqual(current_user.floor, data["floor"])
-        self.assertEqual(current_user.department, data["department"])
-        self.assertEqual(current_user.room_number, data["room_number"])
-        self.assertEqual(current_user.bed, data["bed"])
+        self.assertEqual(current_user.floor, self.data["floor"])
+        self.assertEqual(current_user.department, self.data["department"])
+        self.assertEqual(current_user.room_number, self.data["room_number"])
+        self.assertEqual(current_user.bed, self.data["bed"])
 
-        self.assertEqual(current_user.type_of_diet, data["type_of_diet"])
-        self.assertEqual(current_user.comment, data["comment"])
-        self.assertEqual(current_user.status, data["status"])
+        self.assertEqual(current_user.type_of_diet, self.data["type_of_diet"])
+        self.assertEqual(current_user.comment, self.data["comment"])
+        self.assertEqual(current_user.status, self.data["status"])
 
         self.assertFalse(current_user.is_accompanying)
         self.assertFalse(current_user.is_probe)
         self.assertTrue(current_user.is_without_salt)
         self.assertTrue(current_user.is_without_lactose)
         self.assertFalse(current_user.is_pureed_nutrition)
-        self.assertEqual(current_user.type_pay, data["type_pay"])
+        self.assertEqual(current_user.type_pay, self.data["type_pay"])
         self.assertTrue(current_user.is_change_diet_bd)
         self.assertEqual(current_user.extra_bouillon, "breakfast")
 
@@ -121,3 +128,60 @@ class CustomUserCreateTests(unittest.TestCase):
         count_after_duplicates = CustomUser.objects.count()
         self.assertEqual(count, count_after_duplicates)
         self.assertEqual(response, (None, None))
+
+    def test_is_user_look(self):
+        request = self.factory.post("/doctor/", data=self.data)
+        is_accompanying = False
+        type_pay = None
+        is_probe = False
+        is_without_salt = True
+        is_without_lactose = True
+        is_pureed_nutrition = False
+
+        user_form = PatientRegistrationForm(request.POST)
+        response = is_user_look(
+            user_form,
+            is_accompanying,
+            type_pay,
+            is_probe,
+            is_without_salt,
+            is_without_lactose,
+            is_pureed_nutrition,
+        )
+        self.assertEqual(response, True)
+
+        is_accompanying = True
+
+        wrong_response = is_user_look(
+            user_form,
+            is_accompanying,
+            type_pay,
+            is_probe,
+            is_without_salt,
+            is_without_lactose,
+            is_pureed_nutrition,
+        )
+
+        self.assertEqual(wrong_response, None)
+
+    def test_formatting_full_name_mode_full(self):
+        full_name_row = "Егорова Антонина Степановна"
+        response = formatting_full_name_mode_full(full_name_row)
+
+        self.assertEqual(full_name_row, response)
+
+        full_name_row_1 = "егорова антонина степановна"
+        response_1 = formatting_full_name_mode_full(full_name_row_1)
+        self.assertEqual(full_name_row, response_1)
+
+    def test_update_user_today(self):
+        current_user = CustomUser.objects.last()
+        current_user_full_name = current_user.full_name
+        response = update_UsersToday(current_user)
+        self.assertEqual(response, None)
+
+        current_user_today = UsersToday.objects.get(user_id=current_user.id)
+        self.assertEqual(current_user_today.full_name, current_user_full_name)
+        self.assertEqual(current_user_today.department, current_user.department)
+        self.assertEqual(current_user_today.room_number, current_user.department)
+        self.assertEqual(current_user_today.floor, current_user.floor)
