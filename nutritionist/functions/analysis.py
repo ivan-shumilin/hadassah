b7b61d051263f8bfb4a7c29112fe60763481394f
alice <_@email.com>
2024-02-20 12:22:41 +0300

    поправила статистику

--- nutritionist/functions/analysis.py
+++ nutritionist/functions/analysis.py
@@ -11,7 +11,6 @@ from django.db.models import Count
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hadassah.settings')
 django.setup()
 
-
 from nutritionist.models import Report, CustomUser
 
 
@@ -69,7 +68,7 @@ def create_analysis_2023_12():
     ).values('meal').annotate(total_meal_count=Count('user_id_id', distinct=True))
 
     for meal in reports:
-        total_count_food += meal['total_meal_count']
+        total_count_food += meal['total_meal_count'] # 928
 
     ws.merge_range("A4:B4", 'Общее число рационов', font_title)
     ws.write(3, 2, str(total_count_food), font_table_cell)
@@ -79,27 +78,71 @@ def create_analysis_2023_12():
 
     reports_for_patient = Report.objects.filter(
         date_create__range=(date_start, datetime.date(2024, 1, 1))
-    ).values('date_create', 'user_id_id').distinct('user_id_id')
+    ).values('meal', 'date_create', 'user_id_id')
 
     users = CustomUser.objects.values('id', 'receipt_time', 'receipt_date', 'comment').distinct('id')
 
-    for report in reports_for_patient:
+    for day in range(1, 32):
+        date = datetime.date(2023, 12, day)
 
-        if report['date_create'] != datetime.date(2024, 1, 1):
-            dct_for_patient.setdefault(report['date_create'], [0, 0]) # total and with comments
-            dct_for_patient[report['date_create']][0] += 1
+        # dct = Report.objects.filter(date_create=date).values('meal').annotate(
+        #     num_users=Count('user_id_id', distinct=True))
 
-        user_info = get_user_info(report['user_id_id'], users)
+        dct_to_comments = Report.objects.filter(date_create=date).values('user_id_id').distinct('user_id_id')
 
-        if user_info['comment'] != '':
-            dct_for_patient[report['date_create']][1] += 1
+        total_patient_count_by_day = 0
 
-        if report['date_create'] != datetime.date(2023, 12, 1):
-            if user_info['receipt_time'] >= time(19, 0, 0) and\
-                    (user_info['receipt_date'] == report['date_create'] - timedelta(days=1)):
-                dct_for_patient[report['date_create'] - timedelta(days=1)][0] += 1
+        # for meal in dct:
+        #     total_patient_count_by_day += meal['num_users']
+
+        dct_for_patient.setdefault(date, [0, 0])
+        # dct_for_patient[date][0] += total_patient_count_by_day
+
+        for patient in dct_to_comments:
+            dct_for_patient[date][0] += 1
+            user_info = get_user_info(patient['user_id_id'], users)
+            if user_info['comment'] != '':
+                dct_for_patient[date][1] += 1
+
+            if (user_info['receipt_time'] >= time(19, 0, 0) and
+                    (user_info['receipt_date'] == date - timedelta(days=1)) and
+                    (date - timedelta(days=1) != datetime.date(2023, 11, 30))):
+
+                dct_for_patient[date - timedelta(days=1)][0] += 1
                 if user_info['comment'] != '':
-                    dct_for_patient[report['date_create']][1] += 1
+                    dct_for_patient[date - timedelta(days=1)][1] += 1
+
+    date = datetime.date(2024, 1, 1)
+    dct_to_comments_to_31 = Report.objects.filter(date_create=date).values('user_id_id').distinct('user_id_id')
+
+    for patient in dct_to_comments_to_31:
+        user_info = get_user_info(patient['user_id_id'], users)
+
+        if user_info['receipt_time'] >= time(19, 0, 0) and \
+                (user_info['receipt_date'] == date - timedelta(days=1)):
+            dct_for_patient[date - timedelta(days=1)][0] += 1
+            if user_info['comment'] != '':
+                dct_for_patient[date - timedelta(days=1)][1] += 1
+
+
+
+    # for report in reports_for_patient:
+    #
+    #     if report['date_create'] != datetime.date(2024, 1, 1):
+    #         dct_for_patient.setdefault(report['date_create'], [0, 0])  # total and with comments
+    #         dct_for_patient[report['date_create']][0] += 1
+    #
+    #     user_info = get_user_info(report['user_id_id'], users)
+    #
+    #     if user_info['comment'] != '':
+    #         dct_for_patient[report['date_create']][1] += 1
+    #
+    #     if report['date_create'] != datetime.date(2023, 12, 1):
+    #         if user_info['receipt_time'] >= time(19, 0, 0) and \
+    #                 (user_info['receipt_date'] == report['date_create'] - timedelta(days=1)):
+    #             dct_for_patient[report['date_create'] - timedelta(days=1)][0] += 1
+    #             if user_info['comment'] != '':
+    #                 dct_for_patient[report['date_create']][1] += 1
 
     ws.merge_range("A7:C7", "Число пациентов", font_title)
     ws.write(7, 0, 'Дата', font_table_cell)
@@ -113,7 +156,6 @@ def create_analysis_2023_12():
 
     # (3) Кол-во добавленных позиций через tool корректировки по дням
     # или в целом за месяц (имеются в виду и дополнительные, и в рамках замен)
-
 
     # (4) Кол-во блюд с линии раздачи по дням или в целом за месяц - done
 
@@ -133,8 +175,7 @@ def create_analysis_2023_12():
     for date, count in product_dict_by_date.items():
         ws.write(7 + row, 4, str(date), font_table_cell)
         ws.write(7 + row, 5, count, font_table_cell)
-        row+=1
-
+        row += 1
 
     # (5) Кол-во случаев экстренного питания в целом за месяц без учета поступлений
     # в нерабочие часы (то есть экстренные поступления в рабочие часы + экстренная смена диеты)
