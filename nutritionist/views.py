e4e7b6af98a464c0cefbc0c7dccf3be890014ba5
tothem00n <alisaageeva311@gmail.com>
2024-10-08 00:48:55 +0300

    пофиксила русийикацию дат

--- nutritionist/views.py
+++ nutritionist/views.py
@@ -1906,7 +1906,7 @@ def product_storage_hadassah(request):
     menu = creating_meal_menu_cafe_new(day_of_the_week)
 
     now = datetime.now()
-    locale.setlocale(locale.LC_TIME, "ru_RU")
+    locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 
     # Форматируем дату
     formatted_date = now.strftime('%d')
@@ -1931,7 +1931,7 @@ def product_storage_alcon(request):
     menu = creating_meal_menu_cafe_new(day_of_the_week, alcon=True)
 
     now = datetime.now()
-    locale.setlocale(locale.LC_TIME, "ru_RU")
+    locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 
     # Форматируем дату
     formatted_date = now.strftime('%d')
